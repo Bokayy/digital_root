aeae63d5cff6406bcd62e805700b5c37cbb6a742
Bokayy X <blabsy8@gmail.com>
2023-08-09 13:25:47 +0200

    to i64

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-//make recursive function
-fn recursive(n:i32) -> i32{
-    if n > 1{
-        return n*recursive(n-1);
+fn digital_root(n: i64) -> i64{
+    let mut accumulator: i64 = 0;
+
+    if n.to_string().chars().count() > 1{
+        for letter in n.to_string().chars(){
+            accumulator += letter.to_digit(10).unwrap() as i64;
+        }
     }
-    return 1;
+    return accumulator;
 }
 
-
 fn main() {
-    dbg!(recursive(5));
+    let x = 113;
+    dbg!(digital_root(x));
 }
