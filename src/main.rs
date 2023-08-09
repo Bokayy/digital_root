18277f1f92d62d676d5b9510895cd18b70f56901
Bokayy X <blabsy8@gmail.com>
2023-08-09 17:55:07 +0200

    works

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ fn digital_root(n: i64) -> i64{
             accumulator += letter.to_digit(10).unwrap() as i64;
         }
     }
+    if accumulator.to_string().chars().count() > 1{
+        accumulator = digital_root(accumulator);
+    }
     return accumulator;
 }
 
 fn main() {
-    let x = 113;
+    let x = 132189;
     dbg!(digital_root(x));
 }
